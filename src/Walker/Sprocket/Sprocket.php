d550531d87d391e308822f685e2f2731401b8f6f
Walker Hamilton <myself@walkerhamilton.com>
2013-01-30 14:51:59 -0600

    Initial commit

---
+++ src/Walker/Sprocket/Sprocket.php
@@ -0,0 +1,38 @@
+<?php
+if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+/**
+ * @file
+ * Provide basic concatenation, minifying, and caching of css & js.
+ *
+ * Created by Walker Hamilton on 2013-01-30.
+ */
+
+namespace Walker\Sprocket;
+
+/**
+ * This class provides javascript and css concatenation, minifying, and caching.
+ *
+ * @see https://github.com/sstephenson/sprockets
+ *
+ * @author Walker Hamilton <myself@walkerhamilton.com>
+ * @copyright MIT License
+ * @version 1.0
+ */
+class Sprocket {
+	
+	protected $settings = array(
+		'js_directories' => array(),
+		'css_directories' => array(),
+		'asset_directories' => array(),
+		'cache_directory' => dirname(__FILE__).DS.'cache',
+		'file_path' => null,
+	);
+	
+	protected $constantsScanned = array();
+	protected $constants = array();
+	
+	public function __construct($settings) {
+		$this->settings = array_intersect_key($settings + $this->settings, $this->settings);
+	}
+
+}
